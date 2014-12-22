1b8c20987e0a1ce339eaf33edee4d716c033cc16
Rushaine McBean <rushaine.mcbean@gmail.com>
2014-12-22 02:32:38 -0500

    Setup omniauth routes and twitter auth controller action #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  root "home#index"
+  devise_for :users, :path_names => { sign_in: 'login', sign_out: 'logout' },
+    :controllers => {
+     :omniauth_callbacks => "users/omniauth_callbacks",
+    }
 
+  root "home#index"
 end
