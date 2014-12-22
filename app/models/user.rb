c0df8e5f13713c38066a4d834b73445dfeb8f1ee
Rushaine McBean <rushaine.mcbean@gmail.com>
2014-12-22 02:33:19 -0500

    User model lookups and creation for twitter auth #1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,24 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, 
+  		  :rememberable, :trackable, :validatable, :omniauthable,
+  		  omniauth_providers: [:twitter]
+
+  def self.find_from_twitter_auth(auth)
+  	user = User.where(twitter_username: auth.info.nickname).first
+  end
+
+  def self.create_from_twitter_auth(auth)
+    user = create do |user|
+      user.twitter_username = auth.info.nickname
+      user.uid = auth.uid
+      user.avatar = auth.info.image
+      user.provider = auth.provider
+      user.name = auth.info.name
+      user.password = Devise.friendly_token[0, 20]
+    end
+    user.save(validate: false)
+    user
+  end
 end
