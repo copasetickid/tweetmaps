8b41e42efe5ee022a28c84206aea6f8a106a605f
Rushaine McBean <rushaine.mcbean@gmail.com>
2014-12-24 00:55:51 -0500

    Adds the slug column to the user model #5

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141224045617) do
+ActiveRecord::Schema.define(version: 20141224045903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20141224045617) do
     t.string   "access_token_secret"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
