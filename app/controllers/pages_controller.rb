70945113b2a1eb6e47a73843570fa27a9e472506
Rushaine McBean <rushaine.mcbean@gmail.com>
2014-12-27 13:12:39 -0800

    Redirect signed in users to their dashboard when they access the homepage [closes #9]

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,12 @@ class PagesController < ApplicationController
   before_action :authenticate_user!, only: [:dashboard]
 
   def homepage
+    if user_signed_in?
+      redirect_to dashboard_path(current_user)
+    end
   end
 
   def dashboard
     @user = User.friendly.find(params[:id])
   end
-
 end
