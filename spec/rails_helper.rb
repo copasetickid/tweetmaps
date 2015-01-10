31337235254ecae6f4d00f33d92ee534b014fac6
Rushaine McBean <rushaine.mcbean@gmail.com>
2015-01-10 00:31:56 -0500

    Update user specs  for auth

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,19 +67,14 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before(:each, :js => true) do
-    DatabaseCleaner.strategy = :truncation
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
   end
 
   config.after(:each) do
